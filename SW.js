5a44044689547984b6bcf5c7210aaf2ec8c55b58
Angel Padilla <angelpadillasanchez@hotmail.com>
2021-12-09 10:33:32 -0600

    q

--- SW.js
+++ SW.js
@@ -15,7 +15,10 @@ self.addEventListener("install", (event) => {
         './styles/styles.css',
         'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
         './crearEvento.html',
-        './estilos.css'
+        './estilos.css',
+        'https://code.jquery.com/jquery-3.6.0.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
       ])
     })()
   );
